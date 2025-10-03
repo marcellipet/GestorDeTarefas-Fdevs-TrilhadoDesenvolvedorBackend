62b317e6b858401b972f4b7454fcd7b47e86d748
Marcelli Petranela <marcelli.petranela@upper.net.br>
2025-10-03 16:00:23 -0300

    create: adicionar model Tarefa

---
+++ Models/Tarefa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GestorTarefas.Models
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public DateTime DataCriacao { get; set; } = DateTime.Now;
+        public bool Concluida { get; set; }
+    }
+}
