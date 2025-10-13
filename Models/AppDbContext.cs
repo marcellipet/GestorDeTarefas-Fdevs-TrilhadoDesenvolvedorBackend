1903f3716af1ac0ac483601d748fb2e667d13a9c
Marcelli Petranela <marcelli.petranela@upper.net.br>
2025-10-13 08:56:48 -0300

    configurar AppDbContext e adicionar DbSet<Tarefa>

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace GestorTarefas.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
